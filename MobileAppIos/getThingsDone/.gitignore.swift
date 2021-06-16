ba897d7d77989264be748f42c8318933ac3b6110
Mac 17 <brayan.champi@tecsup.edu.pe>
2021-06-16 15:39:12 -0500

    feat(Validation): Firebase User Login and Validation finished

---
+++ MobileAppIos/getThingsDone/.gitignore.swift
@@ -0,0 +1,8 @@
+//
+//  .gitignore.swift
+//  
+//
+//  Created by Mac 17 on 6/16/21.
+//
+
+import Foundation
---
+++ MobileAppIos/getThingsDone/getThingsDone/DesignableView.swift
@@ -0,0 +1,36 @@
+//
+//  DesignableView.swift
+//  getThingsDone
+//
+//  Created by Mac 17 on 6/15/21.
+//  Copyright © 2021 deah. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
