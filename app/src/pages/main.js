d16ed61f247a97a81f5187fdb294b55591b5edb5
Franco16021998 <62027330+Franco16021998@users.noreply.github.com>
2021-06-18 13:07:23 -0500

    Delete main.js

--- app/src/pages/main.js
+++
@@ -1,4 +0,0 @@
-export default function FirstPost() {
-    return <iframe src="https://calendar.google.com/calendar/embed?src=c_e8vccf6jvcv8utn7eoeu8kjn48%40group.calendar.google.com&ctz=America%2FLima" width="1900" height="930"  ></iframe>
-    
-  }
