a3d900ff9843343b6d8d96803f55713d0bd757a6
Eduardo <xeduardox54@hotmail.com>
2021-06-24 03:04:18 -0500

    Implementando listado y creación de tareas al inbox usando el microservicio diseñado para el mismo | Integrando dependencia cors al microservicio usado

--- app/src/pages/inbox.js
+++ app/src/pages/inbox.js
@@ -2,7 +2,42 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/Layout/layout' // Este componente nos permite mantener un mismo 
 
-export default function Inbox() {
+export async function getStaticProps() {
+    const res = await fetch('http://localhost:3001/api/imbox')
+    const data = await res.json()
+  
+    if (!data) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      }
+    }
+  
+    return {
+      props: { data }, // will be passed to the page component as props
+    }
+}
+
+async function newTask(event) {
+
+    const res = await fetch(
+        'http://localhost:3001/api/imbox',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+                },
+            body: JSON.stringify({
+                "tarea": event.target.tarea.value
+            })
+        }
+    )
+    console.log(res)
+}
+
+export default function Inbox({data}) {
     return (
         <Layout>
             <Head>
@@ -29,25 +64,53 @@ export default function Inbox() {
                     </div>
                     <div className="principal">
                         <div className="wrap">
-                            <form className="formulario" action="agregarTarea">
-                                <input type="text" id="tareaInput" placeholder="Agrega tu tarea"></input>
-                                <button type="button" className="btn btn-primary btn-lg">Agregar Tarea</button>
+                            <form onSubmit={newTask}>
+                                <input type="text" class="tareaInput" placeholder="Agrega tu tarea" name="tarea"></input>
+                                <button class="btnAgregar"></button>
                             </form>
                         </div>
 
                     </div>
-                    <div className="tareas">
-                        <div className="wrap">
-                            <ul className="list-group" id="lista">
-                                <li className="list-group-item">1: Comprar comida para el perro</li>
-                                <li className="list-group-item">2: Realizar la tarea</li>
-                                <li className="list-group-item">3: Llamar a mi padre</li>
+                    <div class="tareas">
+                        <div class="wrap">
+                            <ul class="list-group" id="lista">                      
+                                {data.map((item) => (
+                                    <li class="list-group-item">{item.tarea}</li>
+                                ))}
                             </ul>
                         </div>
                     </div>
 
                 </body>
             </div>
+            <style>{`
+                .principal{
+                    padding: 20px;
+                }
+                .principal form {
+                    display:flex;
+                }
+                .principal .tareaInput{
+                    width: 100%;
+                    margin-right: 10px;
+                    border-color: skyblue!important;
+                    border-radius: 0.5em;
+                }
+                .principal .btnAgregar{
+                    background: url(https://image.flaticon.com/icons/png/512/60/60525.png);
+                    background-size: 100% 100%;
+                    width: 40px;
+                    border-style:none;
+                }
+                .tareas{
+                    padding: 20px;
+                }
+                .tareas .list-group-item{
+                    padding: 10px;
+                    background-color: #ffcf005c;
+                }
+                `}
+            </style>
         </Layout>
     )
 }
